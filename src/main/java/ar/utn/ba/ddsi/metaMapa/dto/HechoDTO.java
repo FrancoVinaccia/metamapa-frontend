6782368e33a2491b34d85ae7a5f558d56cecf30b
FrancoVinaccia <164783682+FrancoVinaccia@users.noreply.github.com>
2025-10-23 21:31:34 -0300

    Add date formatting annotations to fecha field

--- src/main/java/ar/utn/ba/ddsi/metaMapa/dto/HechoDTO.java
+++ src/main/java/ar/utn/ba/ddsi/metaMapa/dto/HechoDTO.java
@@ -1,9 +1,11 @@
 package ar.utn.ba.ddsi.metaMapa.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -20,5 +22,7 @@ public class HechoDTO {
     // Nuevos campos para la vista
     private String categoria;
     private String ubicacion;
-    private LocalDate fecha;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    private LocalDate  fecha;
 }
